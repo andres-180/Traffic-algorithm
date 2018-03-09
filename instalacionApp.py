ab94208a0078c3495bc34d43ccad9a9fc07e776c
andres-180 <belalcazar180@gmail.com>
2018-03-09 11:57:16 -0500

    Add files via upload

--- instalacionApp.py
+++ instalacionApp.py
@@ -12,50 +12,66 @@ os.chdir("Sdk/platform-tools")
 archivo=open("datasetPrueba.txt", "r")
 
 for g in archivo.readlines():
-	instalador="./adb install " + g
-	subprocess.Popen([instalador], shell=True)
-	time.sleep(60)
-	detenerAVD="./adb kill-server"
-	subprocess.call([detenerAVD], shell=True)
-	reconectarHost="./adb reconnect"
-	subprocess.call([reconectarHost], shell=True)
-	reconectarAVD="./adb reconnect device"
-	subprocess.call([reconectarAVD], shell=True)
-	time.sleep(10)
+	try:
+		commandR="./adb install " + g 
+		commandSE=commandR.strip()
+		instalador=commandSE+" > estadoInstalacion.txt"
+		print instalador
+		install=subprocess.call([instalador], shell=True)
+		variableEstado=open("estadoInstalacion.txt", "r")
+		fileLineas=variableEstado.readlines()
+		variableEstado.close()
+		ultimaLinea=fileLineas[len(fileLineas)-1]
+		ultimaLineaSE=ultimaLinea.strip()
+ 		
+		if ultimaLineaSE=="Success":	
+			time.sleep(60)
+			detenerAVD="./adb kill-server"
+			subprocess.call([detenerAVD], shell=True)
+			reconectarHost="./adb reconnect"
+			subprocess.call([reconectarHost], shell=True)
+			reconectarAVD="./adb reconnect device"
+			subprocess.call([reconectarAVD], shell=True)
+			time.sleep(10)
 	
-	#obtencion de paquetes
-	subprocess.Popen(['./adb root'], shell=True)
-	prueba='./adb shell "pm list packages"'
+			#obtencion de paquetes
+			subprocess.Popen(['./adb root'], shell=True)
+			prueba='./adb shell "pm list packages"'
 
-	packages=subprocess.Popen([prueba],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = packages.communicate()
-	f = open('paquetes.txt','w')
-	f.write(out)
-	f.close()
+			packages=subprocess.Popen([prueba],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			out, err = packages.communicate()
+			f = open('paquetes.txt','w')
+			f.write(out)
+			f.close()
 
-	archivoPack=open('paquetes.txt','r')
-	nombreArchivoPartido=g.split('.')
-	nombreArchivo=nombreArchivoPartido[0]
-	for s in archivoPack.readlines():
-		if nombreArchivo in s:
-			paqueteCompleto=s.split(':')
-			nombrePaquete=paqueteCompleto[1]
-			nombrePaquete2=nombrePaquete.strip()
+			archivoPack=open('paquetes.txt','r')
+			nombreArchivoPartido=g.split('.')
+			nombreArchivo=nombreArchivoPartido[0]
+			for s in archivoPack.readlines():
+				if nombreArchivo in s:
+					paqueteCompleto=s.split(':')
+					nombrePaquete=paqueteCompleto[1]
+					nombrePaquete2=nombrePaquete.strip()
 #ejecucion monkey 
-			comandoMonkey="./adb -e shell monkey --ignore-crashes -p "+nombrePaquete2+" 500 ＞ testLogs.txt"
-			subprocess.call([comandoMonkey],shell=True)
+					comandoMonkey="./adb -e shell monkey --ignore-crashes -p "+nombrePaquete2+" -v --throttle 5000 200"
+					subprocess.call([comandoMonkey],shell=True)
 #completar desinstalacion
-			#des="./adb shell pm uninstall -k "+nombrePaquete2
-			#subprocess.call([des],shell=True)
-			#time.sleep(120)			
+					des="./adb shell pm uninstall -k "+nombrePaquete2
+					subprocess.call([des],shell=True)
+					time.sleep(120)			
 			
 			
-			print "finish"
+					print "finish"
 		
 		
 
-archivoPack.close()
-
+			archivoPack.close()
+		else:
+			print "error con la aplicacion"	
+	except IOError as e:
+		t = open("errores.txt", "w")
+		t.write("I/O error({0}): {1}".format(e.errno, e.strerror)+"\n")
+		t.close()
 
 archivo.close()
 
